0ec710e2498e66eb0a2576d040bd3d9a521397dc
Joyce Liao <joyliao07@gmail.com>
2021-08-19 19:31:14 -0700

    add react-owl-carousel

--- src/pages/Volunteer.js
+++ src/pages/Volunteer.js
@@ -3,17 +3,20 @@ import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import SectionBanner from '../components/SectionBanner';
 import PortfolioBlock from '../components/PortfolioBlock';
+import OwlCarousel from 'react-owl-carousel';  
+import 'owl.carousel/dist/assets/owl.carousel.css';
+import 'owl.carousel/dist/assets/owl.theme.default.css';
 
 const Volunteer = () => {
     return (
         <Container>
             <SectionBanner section="Volunteer"/>
-            <Row>
+            <OwlCarousel items={3} className="owl-theme" loop nav margin={8}>
                 <PortfolioBlock name="Temp-1" tech="React JS, ASP.NET"/>
                 <PortfolioBlock name="Temp-2" tech="React JS"/>
-                <PortfolioBlock name="Temp-3" tech="Python3, Google Vision API, PostgreSQL"/>
+                <PortfolioBlock name="Temp-3" tech="Python3, Google Vision"/>
                 <PortfolioBlock name="Temp-4" tech="Angular9, TypeScript"/>
-            </Row>
+            </OwlCarousel>
         </Container>
     );
 }
