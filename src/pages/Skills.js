ee6e8df45e1f8cc807bb318eb52d9761e753c22d
Joyce Liao <joyliao07@gmail.com>
2021-09-09 13:41:58 -0700

    color adjustment

--- src/pages/Skills.js
+++ src/pages/Skills.js
@@ -17,7 +17,7 @@ const Skills = () => {
                     <h1>Developement Experience</h1>
                 </Row>
                 <Row className={classes.description}>
-                    <h4>Joyce is passionate about building apps in various languages, from JavaScript, TypeScript, C#, to Python. She gained experience both from working in the industry and volunteering for tech-for-good projects.</h4>
+                    <h4>Joyce is passionate about building apps in various languages, from JavaScript, TypeScript, C#, to Python. She has gained experience both from working in the industry and volunteering for tech-for-good projects.</h4>
                 </Row>
                 <Row className={classes.buttonRow}>
                     <Nav.Link>
