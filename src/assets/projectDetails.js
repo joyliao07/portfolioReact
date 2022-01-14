3bf9b9cb5d8faa24b682eef805cee03991a4326f
Joyce Liao <joyliao07@gmail.com>
2022-01-14 11:54:17 -0800

    update projects

--- src/assets/projectDetails.js
+++ src/assets/projectDetails.js
@@ -1,9 +1,9 @@
 const projectDetails = [
     {name: "BookFinder", 
     tech:".Net5. React (Typescript)",
-    story:"BookFinder App allows the users to login and search books with a keyword, using Google-Books API. The users can add or remove books from their personal bookshelves, mark favorites, update reading status, and make notes on each book.",
+    story:"BookFinder App allow the user to login and search books with a keyword, using Google-Books API. The user can add or remove books from the personal bookshelf, mark favorites, update reading status, and make notes on each book.",
     git:"https://github.com/joyliao07/jb-jl-booklist ",
-    deploy:"",
+    deploy:"https://bookfinder-react-dotnet.herokuapp.com/",
     color: "(223, 218, 187)",
     colorHover: "(206, 202, 174)"},
     {name: "Portfolio", 
